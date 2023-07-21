2a3a47da7a2f0195bc82f0faf5d488b31c534c9d
HARSH GUPTA <harsh_mkg@yahoo.co.in>
2023-07-21 12:50:35 -0400

    Update src.py

--- src/src.py
+++ src/src.py
@@ -1,90 +1,146 @@
 import json
-from dateutil import rrule
-from dateutil.parser import parse
+from dateutil import rrule, parser
+import heapq
+
+
+class Customer:
+    """
+    A customer object that stores the customer's visit count, image count, total spending, entry and exit event times.
+    """
+
+    def __init__(self):
+        self.visits = 0
+        self.images = 0
+        self.total_spending = 0.0
+        self.event_entry = None
+        self.event_exit = None
+
+    def __repr__(self):
+        return (
+            f"Customer(visits={self.visits}, images={self.images}, total_spending={self.total_spending}, "
+            f"event_entry={self.event_entry}, event_exit={self.event_exit})"
+        )
 
 
 class LTVCalculator:
+    """
+    A class that calculates the LTV of customers.
+    """
+
     def __init__(self):
         self.customers = {}
 
     def ingest(self, event):
-        event_type = event['type']
-        customer_id = event['key'] if event_type == 'CUSTOMER' else event['customer_id']
-        t = parse(event['event_time'])
+        """
+        Ingests an event and updates the LTV of the corresponding customer.
 
+        Args:
+            event (dict): The event to ingest.
+        """
+        # Extract event properties.
+        event_type = event["type"]
+        customer_id = event["key"] if event_type == "CUSTOMER" else event["customer_id"]
+        event_time = parser.parse(event["event_time"])
+
+        # Create or update customer data.
         if customer_id not in self.customers:
-            self.customers[customer_id] = {
-                'visits': 0,
-                'images': 0,
-                'total_spending': 0.0,
-                'event_entry': None,
-                'event_exit': None
-            }
+            self.customers[customer_id] = Customer()
 
         customer = self.customers[customer_id]
 
-        ### exit time is always greater or equal to entry time
-
-        if customer['event_exit']:
-            customer['event_exit'] = t if t > customer['event_exit'] else customer['event_exit']
+        # Update entry and exit event times.
+        if customer.event_exit:
+            customer.event_exit = max(event_time, customer.event_exit)
         else:
-            customer['event_exit'] = t
-        # customer['event_exit'] = parse(event['event_time'])
-        if event_type == 'CUSTOMER':
-            if customer['event_entry'] is None:
-                customer['event_entry'] = parse(event['event_time'])
+            customer.event_exit = event_time
 
-        elif event_type == 'SITE_VISIT':
-            customer['visits'] += 1
+        if event_type == "CUSTOMER":
+            if customer.event_entry is None:
+                customer.event_entry = event_time
 
-        elif event_type == 'IMAGE':
-            customer['images'] += 1
+        elif event_type == "SITE_VISIT":
+            customer.visits += 1
 
-        elif event_type == 'ORDER':
-            customer['total_spending'] += float(event['total_amount'].split(" ")[0])
+        elif event_type == "IMAGE":
+            customer.images += 1
+
+        elif event_type == "ORDER":
+            customer.total_spending += float(event["total_amount"].split(" ")[0])
 
     def calculate_avg_weekly_expenditure(self, customer_id):
+        """
+        Calculates the average weekly expenditure of the customer.
+
+        Args:
+            customer_id (str): The customer ID.
+
+        Returns:
+            float: The average weekly expenditure.
+        """
         customer = self.customers[customer_id]
-        avg_expenditure_per_visit = customer['total_spending'] / customer['visits']
-        weeks = rrule.rrule(rrule.WEEKLY, dtstart=customer['event_entry'], until=customer['event_exit']).count()
-        avg_weekly_visit = customer['visits'] / weeks
+        avg_expenditure_per_visit = customer.total_spending / customer.visits
+        weeks = rrule.rrule(rrule.WEEKLY, dtstart=customer.event_entry, until=customer.event_exit).count()
+        avg_weekly_visit = customer.visits / weeks
         return avg_expenditure_per_visit * avg_weekly_visit
 
     def calculate_top_x_ltv_customers(self, x):
+        """
+        Calculates the top x customers with the highest LTV.
 
+        Args:
+            x (int): The number of customers to return.
+
+        Returns:
+            list: The list of top x customers.
+        """
         top_customers = []
         for customer_id in self.customers:
-            avg_w_exp = self.calculate_avg_weekly_expenditure(customer_id)
-            customer_simple_ltv = avg_w_exp * 52 * 10
-            top_customers.append((customer_id, customer_simple_ltv))
+            # Calculate the Simple LTV.
+            ltv = self.calculate_avg_weekly_expenditure(customer_id) * 52 * 10
+            # Use a min-heap to maintain the top x customers.
+            if len(top_customers) < x:
+                heapq.heappush(top_customers, (ltv, customer_id))
+            else:
+                heapq.heappushpop(top_customers, (ltv, customer_id))
 
-        top_customers.sort(key=lambda x: x[1], reverse=True)
-        return top_customers[:x]
+        # Extract the top x customers from the min-heap.
+        top_customers = [(customer_id, ltv) for ltv, customer_id in heapq.nlargest(x, top_customers)]
+
+        return top_customers
 
 
-def ingest_events(events, D):
+def ingest_events(events, ltv_calculator):
+    """
+    Ingests events into the LTVCalculator.
+
+    Args:
+        events (list): The list of events.
+        ltv_calculator (LTVCalculator): The LTVCalculator.
+    """
     for event in events:
-        D.ingest(event)
+        # Ingest each event into the LTVCalculator.
+        ltv_calculator.ingest(event)
 
 
-def TopXSimpleLTVCustomers(x, D):
-    return D.calculate_top_x_ltv_customers(x)
+def main():
+    ltv_calculator = LTVCalculator()
 
-
-if __name__ == '__main__':
-    D = LTVCalculator()
-
-    with open('input.txt') as infile:
+    with open("input.txt", "r") as infile:
         events = json.load(infile)
 
-    # Ingest events
-    ingest_events(events, D)
+    # Ingest events.
+    ingest_events(events, ltv_calculator)
 
-    x =10
-    # Calculate and print top x customers with highest Simple LTV
-    top_customers = TopXSimpleLTVCustomers(x, D)
+    x = 10
+    # Calculate and print top x customers with the highest Simple LTV.
+    top_customers = ltv_calculator.calculate_top_x_ltv_customers(x)
 
-    with open('output.txt', 'w') as outfile:
-        for customerID, ltv in top_customers:
-            out_str = "Customer ID: " + str(customerID) + ", LTV: " + str(ltv)
-            outfile.write("%s\n" % out_str)
+    with open("output.txt", "w") as outfile:
+        for customer_id, ltv in top_customers:
+            out_str = f"Customer ID: {customer_id}, LTV: {ltv}"
+            outfile.write(f"{out_str}\n")
+
+
+if __name__ == "__main__":
+    main()
+
